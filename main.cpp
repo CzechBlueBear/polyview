36c629057c686f61cd1cb416c41aeba6baeb7456
Jiri Bluebear Dluhos <jiri.bluebear.dluhos@gmail.com>
2024-10-11 16:18:17 +0200

    A primitive form of left/right scrolling.

--- main.cpp
+++ main.cpp
@@ -31,13 +31,15 @@ public:
 
 int main(int argc, char** argv)
 {
+    sdl::InitGuard guard;
+
+    setlocale(LC_ALL,"");
+
     if (argc < 2) {
         std::cerr << "missing argument (file name)\n";
         return 1;
     }
     std::string file_name = argv[1];
-
-    setlocale(LC_ALL,"");
 
     Settings settings;
 
@@ -45,35 +47,41 @@ int main(int argc, char** argv)
     document.load(file_name);
     std::cout << "loaded file: " << file_name << " (" << document.size() << " lines)\n";
 
-    sdl::InitGuard guard;
-
     const std::string FONT_NAME = "/usr/share/fonts/liberation/LiberationMono-Regular.ttf";
     sdl::Font font(FONT_NAME, settings.font_size);
 
-    auto window = std::make_unique<sdl::Window>("Viewer - " + std::string(argv[1]), sdl::Size2d(1024, 1024));
+    auto window = std::make_unique<sdl::Window>("Viewer - " + file_name, sdl::Size2d(1024, 1024));
     auto renderer = std::make_unique<sdl::Renderer>(*window);
 
-    auto current_line = 0u;
+    auto top_visible_line = 0u;
+    auto left_skip = 0u;
     auto lines_visible = renderer->get_output_size().h / font.get_line_skip();
+    auto max_line_width = 0u;
+    const auto HORIZONTAL_SCROLL_AMOUNT = 128;
 
     auto on_redraw = [&] {
-        auto max_width = renderer->get_output_size().w;
-        auto topleft = sdl::Point2d(0, 0);
+        auto viewport_width = renderer->get_output_size().w;
+        max_line_width = 0u;
+        auto topleft = sdl::Point2d(-left_skip, 0);
 
         renderer->fill_rect(sdl::Rect(0, 0, renderer->get_output_size()), settings.background_color);
 
-        for (auto i = current_line; i < std::min(size_t(current_line + lines_visible), document.size()); i++) {
+        for (auto i = top_visible_line; i < std::min(size_t(top_visible_line + lines_visible), document.size()); i++) {
+
+            // render all pieces on the line
             auto line = document.get_line(i);
             for (auto piece : line.pieces) {
                 if (!piece.empty()) {
-                    auto line_texture = renderer->texture_from_surface(
-                        *font.render_wrapped(piece.text, settings.text_color, max_width));
+                    auto line_texture = renderer->texture_from_surface(*font.render(piece.text, settings.text_color));
+                    max_line_width = std::max(max_line_width, line_texture->get_size().w);
                     renderer->put_texture(*line_texture, sdl::Rect(topleft, line_texture->get_size()));
                     topleft.x += line_texture->get_size().w;
                     topleft.x += font.get_space_width();
                 }
             }
-            topleft.x = 0;
+
+            // new line
+            topleft.x = -left_skip;
             topleft.y += font.get_line_skip();
         }
 
@@ -100,13 +108,23 @@ int main(int argc, char** argv)
                     break;
                 }
                 else if (event.key.keysym.sym == SDLK_DOWN) {
-                    if (current_line + lines_visible < document.size()) {
-                        current_line++;
+                    if (top_visible_line + lines_visible < document.size()) {
+                        top_visible_line++;
                     }
                 }
                 else if (event.key.keysym.sym == SDLK_UP) {
-                    if (current_line > 0) {
-                        current_line--;
+                    if (top_visible_line > 0) {
+                        top_visible_line--;
+                    }
+                }
+                else if (event.key.keysym.sym == SDLK_LEFT) {
+                    if (left_skip > 0) {
+                        left_skip -= HORIZONTAL_SCROLL_AMOUNT;
+                    }
+                }
+                else if (event.key.keysym.sym == SDLK_RIGHT) {
+                    if (left_skip < 4096) {
+                        left_skip += HORIZONTAL_SCROLL_AMOUNT;
                     }
                 }
             }
